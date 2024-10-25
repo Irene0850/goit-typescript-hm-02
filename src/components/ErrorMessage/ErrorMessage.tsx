31fda82192809f5cc0ba092166fc6a967f83e383
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-10-25 22:36:30 +0300

    new

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import css from './ErrorMessage.module.css';
+
+interface ErrorMessageProps {
+    message: boolean;
+}
+
+const ErrorMessage: FC<ErrorMessageProps> = ({ message }) => {
+    <div className={css.error}>{message}</div>
+}
+
+export default ErrorMessage;
